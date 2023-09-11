26ffdd8f5183c563af4e3fabb911f043c40a0c8d
carlosroncero <137765152+carlosroncero@users.noreply.github.com>
2023-09-11 20:35:06 +0200

    Update Examenprimertrimestre.java

--- src/main/java/on/croncero/examenprimertrimestre/Examenprimertrimestre.java
+++ src/main/java/on/croncero/examenprimertrimestre/Examenprimertrimestre.java
@@ -13,5 +13,6 @@ public class Examenprimertrimestre {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("hola1");
+        //Está bien ... buen trabajo
     }
 }
